a743272bb78ab03564a272f64ba70bd67e22c0ac
Filip Nilsen <gord1@meme-protocol.click>
2026-03-27 00:44:06 +0000

    Refactored code

--- src/group_29/src/vga_text_mode_interface/vga_text_mode_interface.c
+++ src/group_29/src/vga_text_mode_interface/vga_text_mode_interface.c
@@ -1,25 +1,5 @@
-#pragma once
-//VGA resolution 80 x 25
-struct VgaTextModeCursor{
-    uint8_t row;
-    uint8_t col;
-    uint16_t* memory_position;
-    uint16_t* memory_start;
-    uint16_t* memory_end;
-    void (*CalculateRowColFromMemoryPosition)(struct VgaTextModeCursor*);
-};
+#include "./vga_text_mode_interface.h"
 
-/** Use NewVgaTextModeInterface() to create an instance of this struct */
-struct VgaTextModeInterface{
-    struct VgaTextModeCursor cursor;
-    /** \brief Print a 0 terminated char array to the screen
-     * \param attribute Attribute to set for the text. Applies to the entire string. See enum standard_vga_text_color
-     * \param input An pointer to an array of ASCII characters, terminated by 0.
-    */
-    void (*Print)(struct VgaTextModeInterface*, char* input, uint8_t attribute);
-};
-
-/** \brief See VgaTextModeInterface.Print() */
 void VgaTextModeInterfacePrint(struct VgaTextModeInterface* a, char* input, uint8_t attribute){
     while ( *input != 0 && (a->cursor.memory_position < a->cursor.memory_end)){
         *(a->cursor.memory_position) = (uint16_t)(attribute<<8|*(input));   
@@ -29,16 +9,12 @@ void VgaTextModeInterfacePrint(struct VgaTextModeInterface* a, char* input, uint
     a->cursor.CalculateRowColFromMemoryPosition(&(a->cursor));
 }
 
-/** \brief See VgaTextModeCursor.CalculateRowColFromMemoryPosition()
- * \todo Check that this works correctly
- */
 void VgaTextModeCursorCalculateRowColFromMemoryPosition(struct VgaTextModeCursor* c){
     uint32_t z = (uint32_t)c->memory_position - (uint32_t)c->memory_start;
     c->row = z / 80;
     c->col = z % 80;
 }
 
-/** \brief Initialises a VgaTextModeInterface and returns it. */
 struct VgaTextModeInterface NewVgaTextModeInterface(){
     struct VgaTextModeInterface i;
     i.Print = VgaTextModeInterfacePrint;
@@ -49,4 +25,15 @@ struct VgaTextModeInterface NewVgaTextModeInterface(){
     i.cursor.memory_end = (uint16_t*)((25 * 80 * 2) + 0xb8000);
     i.cursor.CalculateRowColFromMemoryPosition = VgaTextModeCursorCalculateRowColFromMemoryPosition;
     return i;
+}
+
+void copyZeroTerminatedCharArrayToEvenPositionsInCharArray(char* input_array, char* output_array){
+    while ( *input_array != 0 ){
+        *(output_array++) = *(input_array++);
+        output_array++;
+    }
+}
+
+uint8_t VgaColor(enum standard_vga_text_color bg, enum standard_vga_text_color fg){
+    return bg<<4|fg;
 }
