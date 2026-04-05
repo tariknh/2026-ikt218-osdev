81794d96f957598215757b70bf2829fe3a5146bc
marlennr <marlennr@users.noreply.github.com>
2026-04-05 23:27:36 +0000

    assignment 4

--- src/OSDev22/src/kernel.c
+++ src/OSDev22/src/kernel.c
@@ -8,6 +8,10 @@
 #include "isr.h"
 #include "irq.h"
 #include "keyboard.h"
+#include "memory.h"
+#include "pit.h"
+
+extern uint32_t end;
 
 struct multiboot_info {
     uint32_t size;
@@ -30,15 +34,41 @@ int main(uint32_t magic, struct multiboot_info* mb_info_addr) {
     isr_init();     /* Registrer CPU-exceptions (0-31) */
     irq_init();     /* Remap PIC + registrer hardware IRQ-er (32-47) + sti */
 
+    /* Initialiser minnehåndtering */
+    init_kernel_memory(&end);
+    init_paging();
+    print_memory_layout();
+
+    /* Initialiser PIT */
+    init_pit();
+
     /* Start tastatur-driver */
     keyboard_init();
 
     printf("Hello World\n");
     printf("Interrupt-system aktivert!\n");
-    printf("Write something on keybords:\n");
+    printf("Write something on keyboard:\n");
 
-    for (;;) {
-        asm volatile("hlt");
+    /* Test malloc */
+    void* some_memory = malloc(12345);
+    void* memory2 = malloc(54321);
+    void* memory3 = malloc(13331);
+
+    printf("Allocated memory blocks:\n");
+    printf("some_memory = 0x%x\n", (uint32_t)some_memory);
+    printf("memory2     = 0x%x\n", (uint32_t)memory2);
+    printf("memory3     = 0x%x\n", (uint32_t)memory3);
+
+    /* Test PIT */
+    uint32_t counter = 0;
+    while (true) {
+        printf("[%d]: Sleeping with busy-waiting (HIGH CPU).\n", counter);
+        sleep_busy(1000);
+        printf("[%d]: Slept using busy-waiting.\n", counter++);
+
+        printf("[%d]: Sleeping with interrupts (LOW CPU).\n", counter);
+        sleep_interrupt(1000);
+        printf("[%d]: Slept using interrupts.\n", counter++);
     }
 
     return 0;
