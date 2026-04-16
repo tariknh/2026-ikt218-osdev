e305969ffcdd7a1e1513aee691af3a877226d61a
Andreas Lund Helstrom <andreas.lund.helstrom@gmail.com>
2026-04-16 07:55:04 +0200

    Assignment 5: Music Player

--- src/34_OSDev/src/kernel.c
+++ src/34_OSDev/src/kernel.c
@@ -4,8 +4,15 @@
 #include "irq.h"
 #include "memory.h"
 #include "pit.h"
+#include "song.h"
 
 extern uint32_t end;
+
+SongPlayer* create_song_player() {
+    SongPlayer* player = (SongPlayer*)malloc(sizeof(SongPlayer));
+    player->play_song = play_song_impl;
+    return player;
+}
 
 void main(uint32_t mb_magic, void* mb_info) {
     (void)mb_magic; //stop warnings about unused variables
@@ -15,26 +22,34 @@ void main(uint32_t mb_magic, void* mb_info) {
     init_gdt();
     init_idt();
     init_irq();
-    init_kernel_memory(&end);  
-    init_paging();             
-    print_memory_layout();     
-    init_pit();                
+    init_kernel_memory(&end);
+    init_paging();
+    print_memory_layout();
+    init_pit();
 
     __asm__ volatile ("sti"); //enable interrupts after PIC is remapped
 
-    //asm("int $0x1"); //manually trigger interrupt
-
     printf("Hello World\n");
 
-    uint32_t counter = 0;
+    Song songs[] = {
+        {music_1, sizeof(music_1) / sizeof(Note)},
+        {starwars_theme, sizeof(starwars_theme) / sizeof(Note)},
+        {battlefield_1942_theme, sizeof(battlefield_1942_theme) / sizeof(Note)},
+        {music_2, sizeof(music_2) / sizeof(Note)},
+        {music_3, sizeof(music_3) / sizeof(Note)},
+        {music_4, sizeof(music_4) / sizeof(Note)},
+        {music_5, sizeof(music_5) / sizeof(Note)},
+        {music_6, sizeof(music_6) / sizeof(Note)},
+    };
+    uint32_t n_songs = sizeof(songs) / sizeof(Song);
+
+    SongPlayer* player = create_song_player();
 
     while(1) {
-        printf("[%d]: Sleeping with busy-waiting (HIGH CPU).\n", counter);
-        sleep_busy(1000);
-        printf("[%d]: Slept using busy-waiting.\n", counter++);
-
-        printf("[%d]: Sleeping with interrupts (LOW CPU).\n", counter);
-        sleep_interrupt(1000);
-        printf("[%d]: Slept using interrupts.\n", counter++);
-    };
+        for (uint32_t i = 0; i < n_songs; i++) {
+            printf("Playing Song...\n");
+            player->play_song(&songs[i]);
+            printf("Finished playing the song.\n");
+        }
+    }
 }
