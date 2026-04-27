273a7c3e534d2b645d652b83c87a53f1d667d4c3
Peter Vatnar Andersson <peterva@uia.no>
2026-04-27 22:17:45 +0200

    Rename kernel and fix userspace
    Rename kernel

--- src/group_42/src/shell/commands/clear.c
+++
@@ -1,8 +0,0 @@
-#include "drivers/video/vga_text.h"
-
-int cmd_clear(int argc, char** argv) {
-  (void)argc;
-  (void)argv;
-  vga_text_clear_screen();
-  return 0;
-}
--- src/group_42/src/shell/commands/print_memory.c
+++
@@ -1,8 +0,0 @@
-#include <kernel/memory.h>
-
-int cmd_print_memory(int argc, char** argv) {
-  (void)argc;
-  (void)(argv);
-  print_memory_layout();
-  return 0;
-}
